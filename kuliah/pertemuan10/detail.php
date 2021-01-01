febc28694ec8f8629c714f484dff231cd7a5d64e
Veronica <veronicalee.2807@gmail.com>
2021-01-01 13:07:23 +0700

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/detail.php
@@ -0,0 +1,34 @@
+<?php 
+require 'functions.php';
+
+// ambil id dari URL
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("select * from mahasiswa where id = $id");
+// var_dump($mahasiswa['nama']);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+
+    <li><img src="img/<?= $m['gambar']; ?>" width="100" height="100" alt=""></li>
+    <li>NRP: <?= $m['nrp']; ?></li>
+    <li>Nama: <?= $m['nama']; ?></li>
+    <li>Email: <?= $m['email']; ?></li>
+    <li>Jurusan: <?= $m['jurusan']; ?></li>
+    <li><a href="">ubah</a> | <a href="">hapus</a></li>
+    <li><a href="latihan3.php">kembali ke daftar mahasiswa</a></li>
+
+  </ul>
+</body>
+</html>
---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,60 @@
+<?php
+// koneksi ke DB & pilih Database
+$conn = mysqli_connect('localhost', 'root', '', 'pw_2301941611');
+
+// Query isi tabel mahasiswa
+$result = mysqli_query($conn, "select * from mahasiswa");
+// var_dump($result); -> masih dalam bentuk object
+
+// ubah data ke dalam array
+// $row = mysqli_fetch_row($result); -> array numerik
+// $row = mysqli_fetch_assoc($result); -> array associative
+// $row = mysqli_fetch_array($result); -> keduanya
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+// var_dump($rows); -> uda dalam bentuk array associative
+
+// tampung ke variable mahasiswa
+$mahasiswa = $rows;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" width="100" height="100" alt=""></td>
+        <td><?= $m['nrp']; ?></td>
+        <td><?= $m['nama']; ?></td>
+        <td><?= $m['email']; ?></td>
+        <td><?= $m['jurusan']; ?></td>
+        <td>
+          <a href="">ubah</a> | <a href="">hapus</a>
+        </td>
+      </tr>
+      <<?php endforeach; ?> </table> </body> </html>
